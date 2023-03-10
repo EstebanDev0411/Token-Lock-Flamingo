34290a1ba2cfa452962fcce3462d8dbc4a4dedd0
tomio111 <tobilokki99@gmail.com>
2023-03-10 04:33:23 -0800

    initial commit

---
+++ TokenTransfer/TokenTransfer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+
+namespace TokenTransfer
+{
+    [DisplayName("TokenTransfer")]
+    [ManifestExtra("Author", "NEO")]
+    [ManifestExtra("Email", "developer@neo.org")]
+    [ManifestExtra("Description", "This is a TokenTransfer")]
+    public class TokenTransfer : SmartContract
+    {
+        public static bool Main()
+        {
+            return true;
+        }
+    }
+}
